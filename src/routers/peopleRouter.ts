1e985c3b174835d087497e64c14e544b2ab32c13
julrodriguez <brezker10@gmail.com>
2025-08-20 14:17:33 -0600

    crud completo usando arreglo imaginario

--- src/routers/peopleRouter.ts
+++ src/routers/peopleRouter.ts
@@ -12,7 +12,8 @@ router.get('/personas', async (req: Request, res: Response) => {
 
 router.get('/personas/:id', (req: Request, res: Response) => {
   const id = parseInt(req.params.id);
-  const persona = personas[id];
+//   const persona = personas[id]; // para tomar el index del arreglo
+  const persona = personas.find(p => p.id === id);
 
   if (persona) {
     res.json(persona);
@@ -20,5 +21,60 @@ router.get('/personas/:id', (req: Request, res: Response) => {
     res.status(404).json({ error: "Persona no encontrada" });
   }
 });
+
+router.post('/personas', (req: Request, res: Response) => {
+  const nuevaPersona = req.body;
+
+  // Generar un nuevo ID automáticamente
+  const nuevoId = personas.length > 0 ? personas[personas.length - 1].id + 1 : 1;
+  const personaConId = { id: nuevoId, ...nuevaPersona };
+
+  personas.push(personaConId);
+
+  res.status(201).json(personaConId);
+});
+
+// PUT: Reemplazo completo de una persona
+router.put('/personas/:id', (req: Request, res: Response) => {
+  const id = parseInt(req.params.id);
+  const nuevaPersona = req.body;
+
+  const index = personas.findIndex(p => p.id === id); // index revisa long del arreglo
+
+  if (index === -1) {
+    return res.status(404).json({ mensaje: 'Persona no encontrada' });
+  }
+
+  // Reemplazar completamente la persona
+  personas[index] = { id, ...nuevaPersona };
+
+  res.json(personas[index]);
+});
+
+router.delete('/personas/:id', (req: Request, res: Response) => {
+    const id = parseInt(req.params.id);
+    
+    const index = personas.findIndex(p => p.id === id);
+
+    if (index === -1) {
+        return res.status(404).json({ mensaje: 'Persona no encontrada' });
+    }
+
+    // Eliminar la persona del arreglo
+    const personaEliminada = personas.splice(index, 1)[0];
+
+    res.json({ mensaje: 'Persona eliminada', persona: personaEliminada });
+    });
+
+  
+//   const persona = personas.find(p => p.id === id);
+
+//   if (persona) {
+//     const personaEliminada = personas.splice(persona.id, 1)[0];
+//     res.json({ mensaje: 'Persona eliminada', persona: personaEliminada });
+//   } else {
+//     res.status(404).json({ error: "Persona no encontrada" });
+//   }
+// });
 
 export const peopleRouter = router;
